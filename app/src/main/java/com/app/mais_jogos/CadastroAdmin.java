d70f0d2b4b396361e97498bbe8ca6daa67bca5c0
DiogoLCarvalho <diogo.carvalho.lc@gmail.com>
2023-11-16 10:40:43 -0300

    Admin
    Adiciona perfil de administrador

--- app/src/main/java/com/app/mais_jogos/CadastroAdmin.java
+++ app/src/main/java/com/app/mais_jogos/CadastroAdmin.java
@@ -1,7 +1,15 @@
 package com.app.mais_jogos;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
+import android.text.TextPaint;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -11,17 +19,65 @@ public class CadastroAdmin extends AppCompatActivity {
 
     TextView loginPage;
 
+    EditText inputEmail;
+    EditText inputSenha;
+    EditText inputConfirmarSenha;
+    TextView erroInputs;
+    Button btnCadastrar;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_admin);
 
         loginPage = findViewById(R.id.txtLoginAdmin);
+        inputEmail = findViewById(R.id.txtEmailAdmin);
+        inputSenha = findViewById(R.id.txtSenhaAdmin);
+        inputConfirmarSenha = findViewById(R.id.txtSenhaConfirmaAdminLogin);
+        erroInputs = findViewById(R.id.erroCadastroAdmin);
+        btnCadastrar = findViewById(R.id.btnNextAdmin);
+
+
+        /* Title Gradient */
+        TextView titlePage = findViewById(R.id.titleCadastroAdmin);
+        TextPaint paint = titlePage.getPaint();
+        float width = paint.measureText("Cadastre-se");
+
+        Shader textShader = new LinearGradient(0, 0, width, titlePage.getTextSize(), new int[]{
+                        Color.parseColor("#7900FB"),
+                        Color.parseColor("#4B3068"),
+                }, null, Shader.TileMode.CLAMP);
+        titlePage.getPaint().setShader(textShader);
+        /* Title Gradient */
+
+
 
         loginPage.setOnClickListener( e ->{
             Intent gotoLoginPage = new Intent(this, LoginAdmin.class);
 
             startActivity(gotoLoginPage);
         });
+
+        btnCadastrar.setOnClickListener( e ->{
+            if(isInputsCorrected()){
+                Log.i("cadastroAdmin", "Data:\n" + inputEmail.getText().toString() + " " + inputSenha.getText().toString() + " " + inputConfirmarSenha.getText().toString());
+                // Chama o back e cadastra
+
+                Intent intent = new Intent(this, PerfilAdminActivity.class);
+
+                startActivity(intent);
+            }else{
+                Log.i("cadastroAdmin", "tudo errado!");
+                erroInputs.setText("Erro ao preencher as informações!");
+            }
+        });
+    }
+
+    private Boolean isInputsCorrected(){
+        return inputEmail.getText().length() != 0
+                && inputSenha.getText().length() != 0
+                && inputConfirmarSenha.getText().length() != 0
+                && inputSenha.getText().toString().equals(inputConfirmarSenha.getText().toString());
     }
 }
---
+++ app/src/main/java/com/app/mais_jogos/PerfilAdminActivity.java
@@ -0,0 +1,48 @@
+package com.app.mais_jogos;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PerfilAdminActivity extends AppCompatActivity {
+
+    Button excluirAdmin;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.perfil_admin);
+
+        excluirAdmin = findViewById(R.id.btnExcluirAdmin);
+
+        excluirAdmin.setOnClickListener( e ->{
+            // Modal
+            AlertDialog.Builder confirmaExlusao = new AlertDialog.Builder(PerfilAdminActivity.this);
+            confirmaExlusao.setTitle("Atenção!!");
+            confirmaExlusao.setMessage("Tem certeza que deseja excluir a sua conta?\nEssa ação não pode ser desfeita!");
+            confirmaExlusao.setCancelable(false);
+
+            confirmaExlusao.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    // Conecta com Back e exclui
+
+                    Intent intent = new Intent(getApplicationContext(), SelectPlayer.class);
+
+                    startActivity(intent);
+                }
+            });
+
+            confirmaExlusao.setNegativeButton("Não", null);
+
+            confirmaExlusao.create().show();
+        });
+
+    }
+}
